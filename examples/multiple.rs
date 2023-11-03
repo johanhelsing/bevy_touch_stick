a92f44f6d1aa169c164801e78f4a175dd4de0e78
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2023-11-03 09:56:30 +0100

    Clean up multiple example

--- examples/multiple.rs
+++ examples/multiple.rs
@@ -4,53 +4,61 @@ use bevy_touch_stick::*;
 
 // ID for joysticks
 #[derive(Default, Reflect, Hash, Clone, PartialEq, Eq)]
-enum JoystickController {
+enum Stick {
     #[default]
-    LeftStick,
-    RightStick,
+    Left,
+    Right,
 }
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(WorldInspectorPlugin::new())
-        .add_plugins(TouchStickPlugin::<JoystickController>::default())
+        .add_plugins((
+            DefaultPlugins,
+            WorldInspectorPlugin::new(),
+            TouchStickPlugin::<Stick>::default(),
+        ))
         .add_systems(Startup, create_scene)
-        .add_systems(Update, update_joystick)
+        .add_systems(Update, (move_player, update_stick_colors))
         .run();
 }
 
 #[derive(Component)]
 // Player with velocity
-struct Player(pub f32);
+struct Player {
+    pub max_speed: f32,
+}
 
-fn create_scene(mut cmd: Commands, asset_server: Res<AssetServer>) {
-    cmd.spawn(Camera2dBundle {
+fn create_scene(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle {
         transform: Transform::from_xyz(0., 0., 5.0),
         ..default()
     });
-    cmd.spawn(SpriteBundle {
-        transform: Transform {
-            translation: Vec3::new(0., 0., 0.),
+
+    commands.spawn((
+        Player { max_speed: 50. },
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(0., 0., 0.),
+                ..default()
+            },
+            texture: asset_server.load("knob.png"),
+            sprite: Sprite {
+                color: Color::PURPLE,
+                custom_size: Some(Vec2::new(50., 50.)),
+                ..default()
+            },
             ..default()
         },
-        texture: asset_server.load("knob.png"),
-        sprite: Sprite {
-            color: Color::PURPLE,
-            custom_size: Some(Vec2::new(50., 50.)),
-            ..default()
-        },
-        ..default()
-    })
-    .insert(Player(50.));
+    ));
+
     // Spawn Virtual Joystick on left
-    cmd.spawn(
+    commands.spawn((
         TouchStickBundle::new(TouchStickNode {
             border_image: asset_server.load("outline.png"),
             knob_image: asset_server.load("knob.png"),
             knob_size: Vec2::new(80., 80.),
             dead_zone: 0.,
-            id: JoystickController::LeftStick,
+            id: Stick::Left,
             behavior: TouchStickType::Fixed,
         })
         .set_color(TintColor(Color::WHITE.with_a(0.2)))
@@ -62,17 +70,17 @@ fn create_scene(mut cmd: Commands, asset_server: Res<AssetServer>) {
             bottom: Val::Percent(15.),
             ..default()
         }),
-    )
-    .insert(BackgroundColor(Color::ORANGE_RED.with_a(0.2)));
+        BackgroundColor(Color::ORANGE_RED.with_a(0.2)),
+    ));
 
     // Spawn Virtual Joystick on Right
-    cmd.spawn(
+    commands.spawn((
         TouchStickBundle::new(TouchStickNode {
             border_image: asset_server.load("outline.png"),
             knob_image: asset_server.load("knob.png"),
             knob_size: Vec2::new(80., 80.),
             dead_zone: 0.,
-            id: JoystickController::RightStick,
+            id: Stick::Right,
             behavior: TouchStickType::Fixed,
             ..default()
         })
@@ -85,44 +93,48 @@ fn create_scene(mut cmd: Commands, asset_server: Res<AssetServer>) {
             bottom: Val::Percent(15.),
             ..default()
         }),
-    )
-    .insert(BackgroundColor(Color::ORANGE_RED.with_a(0.2)));
+        BackgroundColor(Color::ORANGE_RED.with_a(0.2)),
+    ));
 }
 
-fn update_joystick(
-    mut joystick: EventReader<TouchStickEvent<JoystickController>>,
-    mut joystick_color: Query<(&mut TintColor, &TouchStickNode<JoystickController>)>,
-    mut player: Query<(&mut Transform, &Player)>,
-    time_step: Res<FixedTime>,
+fn update_stick_colors(
+    mut stick_events: EventReader<TouchStickEvent<Stick>>,
+    mut sticks: Query<(&mut TintColor, &TouchStickNode<Stick>)>,
 ) {
-    let (mut player, player_data) = player.single_mut();
+    for event in stick_events.iter() {
+        let tint_color = match event.get_type() {
+            TouchStickEventType::Press | TouchStickEventType::Drag => TintColor(Color::WHITE),
+            TouchStickEventType::Release => TintColor(Color::WHITE.with_a(0.2)),
+        };
 
-    for j in joystick.iter() {
-        let Vec2 { x, y } = j.value();
-
-        match j.get_type() {
-            TouchStickEventType::Press | TouchStickEventType::Drag => {
-                for (mut color, node) in joystick_color.iter_mut() {
-                    if node.id == j.id() {
-                        *color = TintColor(Color::WHITE);
-                    }
-                }
-            }
-            TouchStickEventType::Release => {
-                for (mut color, node) in joystick_color.iter_mut() {
-                    if node.id == j.id() {
-                        *color = TintColor(Color::WHITE.with_a(0.2));
-                    }
-                }
+        for (mut color, node) in &mut sticks {
+            if node.id == event.id() {
+                *color = tint_color;
             }
         }
+    }
+}
 
-        match j.id() {
-            JoystickController::LeftStick => {
-                player.translation.x += x * player_data.0 * time_step.period.as_secs_f32();
+fn move_player(
+    // todo: don't use events
+    mut stick_events: EventReader<TouchStickEvent<Stick>>,
+    mut players: Query<(&mut Transform, &Player)>,
+    time_step: Res<FixedTime>,
+) {
+    let (mut player_transform, player) = players.single_mut();
+
+    for event in stick_events.iter() {
+        let Vec2 { x, y } = event.value();
+
+        // todo: maybe it's more interesting to set player direction per stick instead?
+        match event.id() {
+            Stick::Left => {
+                player_transform.translation.x +=
+                    x * player.max_speed * time_step.period.as_secs_f32();
             }
-            JoystickController::RightStick => {
-                player.translation.y += y * player_data.0 * time_step.period.as_secs_f32();
+            Stick::Right => {
+                player_transform.translation.y +=
+                    y * player.max_speed * time_step.period.as_secs_f32();
             }
         }
     }
